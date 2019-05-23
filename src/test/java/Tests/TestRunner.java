8584318b69448be426cf4da7f1a9360ee7ff9e03
msguner <msguner55@gmail.com>
2019-05-23 18:40:33 +0300

    add runner

---
+++ src/test/java/Tests/TestRunner.java
@@ -0,0 +1,47 @@
+package runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.PickleEventWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@CucumberOptions(
+        features = {"classpath:features"},
+        glue = "steps",
+        tags = {"@login"},
+        plugin = {"pretty",
+                "json:target/cucumber.json"
+//                "html:target/cucumber-reports/cucumber-pretty",
+//                "html:target/cucumber-reports/cucumber-html-report",
+//                "json:target/cucumber-reports/CucumberTestReport.json",
+//                "rerun:target/cucumber-reports/rerun.txt",
+        },
+        monochrome = false)
+
+public class TestRunner {
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
+    public void scenario(PickleEventWrapper pickleEvent, CucumberFeatureWrapper cucumberFeature) throws Throwable {
+        testNGCucumberRunner.runScenario(pickleEvent.getPickleEvent());
+    }
+
+    @DataProvider
+    public Object[][] scenarios() {
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
+}
